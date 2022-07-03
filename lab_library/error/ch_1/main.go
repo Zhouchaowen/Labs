f865603d6e31eaf4d500a017383c2669c319a6d2
zhouchaowen <=>
2022-07-03 13:56:08 +0800

    modify error lab

---
+++ lab_library/error/ch_1/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type errorString string
+
+func (e errorString) Error() string {
+	return string(e)
+}
+
+func New(text string) error {
+	return errorString(text)
+}
+
+var ErrNamedType = New("EOF")
+var ErrStructType = errors.New("EOF")
+
+/*
+自定义Err的坑
+func New(text string) error {
+	return &errorString{text}  返回指针
+}
+ */
+func main() {
+	if ErrNamedType == New("EOF"){
+		fmt.Println("Named Type Error")
+	}
+
+	if ErrStructType == errors.New("EOF"){
+		fmt.Println("Struct Type Error")
+	}
+}
---
+++ lab_library/error/ch_2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Positive(n int) (bool,error) {
+	if n == 0 {
+		return false,errors.New("undefined")
+	}
+	return n > -1,nil
+}
+
+func Check(n int)  {
+	pos ,err := Positive(n)
+	if err != nil {
+		fmt.Println("is neither")
+		return
+	}
+	if pos {
+		fmt.Println("is positive")
+	}else {
+		fmt.Println("is negative")
+	}
+}
+
+/*
+	异常和错误是两回事
+
+	exceptions 破坏语义
+
+	简单
+	考虑失败，而不是成功（plan for failure, not success）
+	没有隐藏的控制流
+	完全交给你来控制 error
+	Error are values
+
+ */
+func main() {
+	Check(1)
+	Check(0)
+	Check(-1)
+}
