dcc80a40e5ae93a04b1b43bbafe5fb08b00b213c
zhouchaowen <=>
2022-11-01 23:47:06 +0800

    add sync mutex

---
+++ lab_library/sync/mutex/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func UnSafeAdd() {
+	var count = 0
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 10000; j++ {
+				count++
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(count)
+}
+
+func SafeAdd() {
+	var mu sync.Mutex
+	var count = 0
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 10000; j++ {
+				mu.Lock()
+				count++
+				mu.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(count)
+}
+
+func main() {
+	UnSafeAdd()
+	SafeAdd()
+}
---
+++ lab_library/sync/mutex/spinlock.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+type spinLock uint32
+
+func (sl *spinLock) Lock() {
+	for !atomic.CompareAndSwapUint32((*uint32)(sl), 0, 1) {
+		runtime.Gosched() //without this it locks up on GOMAXPROCS > 1
+	}
+}
+
+func (sl *spinLock) Unlock() {
+	atomic.StoreUint32((*uint32)(sl), 0)
+}
+
+func (sl *spinLock) TryLock() bool {
+	return atomic.CompareAndSwapUint32((*uint32)(sl), 0, 1)
+}
+
+func SpinLock() sync.Locker {
+	var lock spinLock
+	return &lock
+}
