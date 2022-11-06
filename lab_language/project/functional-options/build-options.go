f2df4feac53c00a4022cd95efee7584e8fbc98ac
zhouchaowen <=>
2022-11-06 23:22:54 +0800

    add some labs to project

---
+++ lab_language/project/functional-options/build-options.go
@@ -0,0 +1,61 @@
+package functional_options
+
+import (
+	"crypto/tls"
+	"time"
+)
+
+type Server struct {
+	Addr     string
+	Port     int
+	Protocol string
+	Timeout  time.Duration
+	MaxConns int
+	TLS      *tls.Config
+}
+
+//使用一个builder类来做包装
+type ServerBuilder struct {
+	Server
+}
+
+func (sb *ServerBuilder) Create(addr string, port int) *ServerBuilder {
+	sb.Server.Addr = addr
+	sb.Server.Port = port
+	//其它代码设置其它成员的默认值
+	return sb
+}
+
+func (sb *ServerBuilder) WithProtocol(protocol string) *ServerBuilder {
+	sb.Server.Protocol = protocol
+	return sb
+}
+
+func (sb *ServerBuilder) WithMaxConn(maxconn int) *ServerBuilder {
+	sb.Server.MaxConns = maxconn
+	return sb
+}
+
+func (sb *ServerBuilder) WithTimeOut(timeout time.Duration) *ServerBuilder {
+	sb.Server.Timeout = timeout
+	return sb
+}
+
+func (sb *ServerBuilder) WithTLS(tls *tls.Config) *ServerBuilder {
+	sb.Server.TLS = tls
+	return sb
+}
+
+func (sb *ServerBuilder) Build() Server {
+	return sb.Server
+}
+
+func mainBuildOptions() {
+	sb := ServerBuilder{}
+	server := sb.Create("127.0.0.1", 8080).
+		WithProtocol("udp").
+		WithMaxConn(1024).
+		WithTimeOut(30 * time.Second).
+		Build()
+	_ = server
+}
